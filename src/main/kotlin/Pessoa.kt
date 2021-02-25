4bc607f8f54088a3d828622ec4ab5cf2188c6521
Felipe Henrique <fheehenrique@gmail.com>
2021-02-25 19:00:37 -0300

    update to apply the private modifier to set function of cpf property, updated to talk about class members

--- src/main/kotlin/Pessoa.kt
+++ src/main/kotlin/Pessoa.kt
@@ -2,11 +2,14 @@ class Pessoa {
     var nome = "Felipe"
     var cpf = "458.657.487-58"
     private set
+
+    constructor()
+
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main() {
     val Felipe = Pessoa()
+    println(Felipe.pessoaInfo())
 
-    println(Felipe.nome)
-    println(Felipe.cpf)
 }
