7240754bce2bcb380632c0c8e4378ab80c7e15f2
Felipe Henrique <fheehenrique@gmail.com>
2021-02-25 21:43:48 -0300

    created sub classes manager and analist

---
+++ src/main/kotlin/one/digitalinnovation/digioonebank/Gerente.kt
@@ -0,0 +1,8 @@
+package one.digitalinnovation.digioonebank
+
+class Gerente(nome: String,
+              cpf: String,
+              salario: Double
+) : Funcionario(nome = nome, cpf = cpf, salario = salario) {
+    override fun calculoAuxilio(): Double = salario * 0.4
+}
