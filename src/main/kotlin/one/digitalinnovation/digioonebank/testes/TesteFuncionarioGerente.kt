b3a52b8b20666c02207221052730a396870bdeee
Felipe Henrique <fheehenrique@gmail.com>
2021-02-25 23:42:13 -0300

    updated manager and test manager

--- src/main/kotlin/one/digitalinnovation/digioonebank/testes/TesteFuncionarioGerente.kt
+++ src/main/kotlin/one/digitalinnovation/digioonebank/testes/TesteFuncionarioGerente.kt
@@ -4,6 +4,9 @@ import one.digitalinnovation.digioonebank.Gerente
 
 
 fun main() {
-    val maria = Gerente(nome = "Maria do Carmo", cpf = "123.456.789-10", salario = 5000.0)
+    val maria = Gerente(nome = "Maria do Carmo", cpf = "123.456.789-10", salario = 5000.0, senha = "senha123")
+
     imprimeRelatorioFuncionario.imprime(maria)
+
+    TesteAutenticacao().autentica(maria)
 }
