2a742c78fd2795ce4d961106d3cc0806d8569cad
Felipe Henrique <fheehenrique@gmail.com>
2021-02-25 21:02:09 -0300

    updates to make person and employee abstract class

--- src/main/kotlin/one/digitalinnovation/digioonebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digioonebank/Pessoa.kt
@@ -1,17 +1,6 @@
 package one.digitalinnovation.digioonebank
 
-class Pessoa {
-    var nome = "Felipe"
-    var cpf = "458.657.487-58"
-    private set
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main() {
-    val Felipe = Pessoa()
-    println(Felipe.pessoaInfo())
-
-}
+abstract class Pessoa (
+    val nome: String,
+    val cpf: String
+)
